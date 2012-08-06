c83c00e7c3f7a0b27f4300d3719851f1b4bec7d5
Ngo Thanh Nghi <ngothanhnghi@gmail.com>
2012-08-06 20:29:16 +0800

    Create user class

---
+++ MeetingScheduler/User.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   User.cpp
+ * Author: ngothanhnghi
+ * 
+ * Created on August 6, 2012, 8:28 PM
+ */
+
+#include "User.h"
+
+User::User() {
+}
+
+User::User(const User& orig) {
+}
+
+User::~User() {
+}
+
---
+++ MeetingScheduler/User.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   User.h
+ * Author: ngothanhnghi
+ *
+ * Created on August 6, 2012, 8:28 PM
+ */
+
+#ifndef USER_H
+#define	USER_H
+
+class User {
+public:
+    User();
+    User(const User& orig);
+    virtual ~User();
+private:
+
+};
+
+#endif	/* USER_H */
+
