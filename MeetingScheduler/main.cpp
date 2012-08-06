6b5bd3566be9b73a73de982b87bd22abc913e571
thanhnghi.ngo <thanhnghi.ngo@localhost6.localdomain6>
2012-08-06 11:30:19 +0800

    Project initialize

---
+++ MeetingScheduler/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: thanhnghi.ngo
+ *
+ * Created on August 6, 2012, 11:29 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
