537b1a7f77e4088694594ffb4660ecfdc3b3fc5a
Ngo Thanh Nghi <ngothanhnghi@gmail.com>
2012-08-06 20:31:54 +0800

    create role class

---
+++ MeetingScheduler/Role.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Role.cpp
+ * Author: ngothanhnghi
+ * 
+ * Created on August 6, 2012, 8:31 PM
+ */
+
+#include "Role.h"
+
+Role::Role() {
+}
+
+Role::Role(const Role& orig) {
+}
+
+Role::~Role() {
+}
+
---
+++ MeetingScheduler/Role.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Role.h
+ * Author: ngothanhnghi
+ *
+ * Created on August 6, 2012, 8:31 PM
+ */
+
+#ifndef ROLE_H
+#define	ROLE_H
+
+class Role {
+public:
+    Role();
+    Role(const Role& orig);
+    virtual ~Role();
+private:
+
+};
+
+#endif	/* ROLE_H */
+
